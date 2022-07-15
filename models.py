67945c4d983a87eb75f6f55c533ac6ef6f5e9ed8
Michael Lenane <michael.j.lenane@gmail.com>
2022-07-14 23:04:18 -0400

    Created 'users' model that establishes columns in database

--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+
+
+class Users (db.Model):
+    __table_name__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(20),
+                           nullable=False,)
+
+    last_name = db.Column(db.String(20),
+                          nullable=False)
+
+    image_url = db.Column(db.URL,
+                          nullable=True,
+                          default="https://www.istockphoto.com/photos/profile-avatar")
